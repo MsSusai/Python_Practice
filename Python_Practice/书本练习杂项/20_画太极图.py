1519cb5abd45404789fc000a82d0a5b13faeaf3b
MsSusai <liuhaorantony@163.com>
2020-11-19 21:09:39 +0800

    commit

--- Python_Practice/书本练习杂项/20_画太极图.py
+++ Python_Practice/书本练习杂项/20_画太极图.py
@@ -1,49 +1,27 @@
 from turtle import *
 
 color('black', 'black')
-
+width(3)
 begin_fill()
-
 circle(-50, 180)
-
 circle(50, 180)
-
 circle(100, 180)
-
 pu()
-
 left(90)
-
 fd(40)
-
 right(90)
-
 circle(10)
-
 end_fill()
-
 color('black', 'white')
-
 left(90)
-
 fd(100)
-
 pd()
-
 right(90)
-
 begin_fill()
-
 circle(10)
-
 end_fill()
-
 pu()
-
 home()
-
 pd()
-
 circle(-100, -180)
-
 hideturtle()
