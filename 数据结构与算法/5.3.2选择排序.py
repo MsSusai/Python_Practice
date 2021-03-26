2d8929222e7175daa5762ffd801e29a3bc9abd73
MsSusai <liuhaorantony@163.com>
2021-03-26 16:29:17 +0800

    commit

--- 数据结构与算法/5.3.2选择排序.py
+++ 数据结构与算法/5.3.2选择排序.py
@@ -1,13 +1,15 @@
 def selectSort(alist):
-    for i in range(len(alist) - 1, 0, -1):
+    total = len(alist)
+    for i in range(len(alist) - 1):
         positionMax = 0
-        for location in range(1, i + 1):
+        for location in range(1, total):
             if alist[location] > alist[positionMax]:
                 positionMax = location
 
-        alist[positionMax], alist[i] = alist[i], alist[positionMax]
+        alist[positionMax], alist[total - 1] = alist[total - 1], alist[positionMax]
+        total = total - 1
 
 
-test = [1, 5, 6, 2, 3, 2]
+test = [1, 5, 6, 2, 3, 2, 10, 6]
 selectSort(test)
 print(test)
