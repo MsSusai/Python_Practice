95726902a8e5fc6d7fff454e09f256eddb699483
MsSusai <liuhaorantony@163.com>
2021-07-12 19:47:08 +0800

    commit

--- 机器学习/test.py
+++ 机器学习/test.py
@@ -5,13 +5,13 @@
 # 名称：test.PY
 # 工具：PyCharm
 import torch
-import tensorflow as tf
+# import tensorflow as tf
 
-# x = torch.rand(5, 3)
-# print(x)
-# print(torch.cuda.is_available())
+x = torch.rand(5, 3)
+print(x)
+print(torch.cuda.is_available())
+
 # print(tf.reduce_sum(tf.random.normal([1000, 1000])))
-
-hello = tf.constant('Hello tensorflow')
-sess = tf.Session()
-print(sess.run(hello))
+# hello = tf.constant('Hello tensorflow')
+# sess = tf.Session()
+# print(sess.run(hello))
