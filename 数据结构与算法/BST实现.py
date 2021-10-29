f8d38a797d1ca1e70e6cdadf7ae89db59952bf62
MsSusai <liuhaorantony@163.com>
2021-10-29 13:03:46 +0800

    commit

--- 数据结构与算法/BST实现.py
+++ 数据结构与算法/BST实现.py
@@ -15,14 +15,16 @@ class TreeNode:
 class BinarySearchTree:
     def __init__(self, valList):
         self.root = None
-        node = self.root = self.insertNode(self.root, valList[0])
-        for i in range(1, len(valList)):
+        # 构造BST的方法
+        node = self.root = self.insertNode(self.root, valList[0])  # 标记根节点，便于后续第一个传入根节点
+        for i in range(1, len(valList)):  # 去除根节点使用过的数
             node = self.insertNode(node, valList[i])
 
+    # BST搜索
     def searchNode(self, node, val):
-        if node is None:
+        if node is None:  # 树为空
             return None
-        else:
+        else:  # 树不为空
             if val == node.data:
                 return node
             elif val > node.data:
@@ -33,15 +35,16 @@ class BinarySearchTree:
                 raise "搜索时发生错误"
         return find
 
+    # BST插入
     def insertNode(self, node, val):
-        if node is None:
+        if node is None:  # 创造节点
             node = TreeNode(val)
-        elif val > node.data:
-            node.rightChild = self.insertNode(node.rightChild, val)
-            node.rightChild.parent = node
-        elif val < node.data:
-            node.leftChild = self.insertNode(node.leftChild, val)
-            node.leftChild.parent = node
+        elif val > node.data:  # 值大于节点
+            node.rightChild = self.insertNode(node.rightChild, val)  # node.rightChild = self...是为了连接父节点的右子节点
+            node.rightChild.parent = node  # 将右子节点的父节点标记上
+        elif val < node.data:  # 值小于节点
+            node.leftChild = self.insertNode(node.leftChild, val)  # node.leftChild = self...是为了连接父节点的左子节点
+            node.leftChild.parent = node  # 将左子节点的父节点标记上
         return node
 
     def postOrder(self, root):
